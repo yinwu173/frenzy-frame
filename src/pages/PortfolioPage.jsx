457b46a3165e7624e2c6dfaf88e7fd39d39b77f4
yinwu173 <yin.wu173@gmail.com>
2024-11-19 20:37:54 -0800

    added basic formating to Portfolio page

---
+++ src/pages/PortfolioPage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function PortfolioPage() {
+    return(
+        <section>
+            <div>
+
+            </div>
+        </section>
+    )
+}
+
+export default PortfolioPage;
