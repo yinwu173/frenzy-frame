9dee32ce5f824ef5e4383b36f85039557e72e5df
yinwu173 <yin.wu173@gmail.com>
2024-11-19 19:21:47 -0800

    added Header file: moved header and heading 1 code from NavTabs.jsx to Header.jsx then exported Header file

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+function Header() {
+    return(
+        <header>
+            <div>
+                <h1>Yin Wu's React Portfolio</h1>
+            </div>
+        </header>
+    ); 
+}
+
+export default Header;
