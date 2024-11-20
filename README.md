c1ba9d90b4b47a973411f32c06838d6a8882abfb
yinwu173 <yin.wu173@gmail.com>
2024-11-19 19:59:13 -0800

    Created Footer component: added clickable link for Github and LinkedIn

---
+++ README.md
@@ -0,0 +1,3 @@
+### Resources
+
+* [React External Hyperlink Resource](https://stackoverflow.com/questions/50350085/how-to-make-a-hyperlink-external-in-react)
---
+++ src/components/Footer.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <footer>
+            <div>
+                <a
+                    href="https://github.com/yinwu173/"
+                    target="_blank" 
+                    rel="noopener noreferrer"                
+                >
+                    Github Link
+                </a>
+                <a
+                    href="https://www.linkedin.com/in/yin-wu-021573178/"
+                    target="_blank" 
+                    rel="noopener noreferrer"                
+                >
+                    LinkedIn
+                </a>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
