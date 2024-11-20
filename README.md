fb039886a4c030a1b2a7684d5af8347c6828a3f0
yinwu173 <yin.wu173@gmail.com>
2024-11-19 20:00:02 -0800

    edited readme: added resources for the react awesome git hub and linked in icons

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ### Resources
 
 * [React External Hyperlink Resource](https://stackoverflow.com/questions/50350085/how-to-make-a-hyperlink-external-in-react)
+
+* [How to Use Font Awesome in React](https://stackoverflow.com/questions/23116591/how-to-include-a-font-awesome-icon-in-reacts-render)
+
+* [Font Awesome Github Icon in React](https://react-icons.github.io/react-icons/search/#q=github)
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 function Footer() {
     return (
@@ -9,14 +10,14 @@ function Footer() {
                     target="_blank" 
                     rel="noopener noreferrer"                
                 >
-                    Github Link
+                    Github <FaGithub size={20} />
                 </a>
                 <a
                     href="https://www.linkedin.com/in/yin-wu-021573178/"
                     target="_blank" 
                     rel="noopener noreferrer"                
                 >
-                    LinkedIn
+                    LinkedIn <FaLinkedin size={20} />
                 </a>
             </div>
         </footer>
