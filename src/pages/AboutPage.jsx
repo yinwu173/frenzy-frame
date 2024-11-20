d9b6cc7e4ca569e1a227605c886563464468c0df
yinwu173 <yin.wu173@gmail.com>
2024-11-19 20:20:59 -0800

    edited AboutPage.jsx: created a section, header, and paragraph to introduce myself to the user.

---
+++ src/pages/AboutPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function AboutPage() {
+    return(
+        <section>
+            <h2>About Me</h2>
+            <p>
+                Hello ~ My name is Yin and welcome to my React Portfolio. 
+                I am a full stack web develop who enjoys learning about 
+                new technologies and creating user friendly interfaces. 
+            </p>
+        </section>
+    );
+}
+
+export default AboutPage;
