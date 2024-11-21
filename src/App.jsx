71d89a56dcd4c57e5006ba76e5d03fb7ad6aef2c
yinwu173 <yin.wu173@gmail.com>
2024-11-20 20:38:19 -0800

    imported components and pages + current page is active + handle page change updates the current page + render the page + export the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+
+import App from './App';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Portfolio from './components/Portfolio';
+import AboutPage from "./pages/AboutPage";
+import ResumePage from "./pages/ResumePage";
+import ContactPage from "./pages/ContactPage";
+
 
 function App() {
-  return (
-    <div>
-      <h1>Welcome to the portfolio!</h1>
-    </div>
-  );
+    const [currentPage, setCurrentPage] = useState('About');
+
+    const renderPage = () => {
+        if (currentPage === 'About') {
+            return <AboutPage />;
+        }
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+        if (currentPage === 'Resume') {
+            return <ResumePage />;
+        }
+        if (currentPage === 'Contact') {
+            return <ContactPage />;
+        }
+        return null;
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
+
+    return (
+        <div>
+            <Header currentPage={currentPage} handlePageChange={handlePageChange} />
+            <div>
+                <main>{renderPage()}</main>
+            </div>
+            <Footer />
+        </div>
+    );
 }
 
-export default App; 
+export default App;
